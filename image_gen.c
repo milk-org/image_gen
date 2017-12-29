b7d2626de6361dae31b1933c4f3d25298ff0d225
oguyon <oliv.guyon@gmail.com>
2017-12-28 22:02:44 -1000

    msync

--- image_gen.c
+++ image_gen.c
@@ -195,7 +195,12 @@ int_fast8_t image_gen_im2coord_cli(){
 void __attribute__ ((constructor)) libinit_image_gen()
 {	
 	init_image_gen();
-//	printf(" ...... Loading module %s\n", __FILE__);
+
+	if(data.progStatus>0)
+	{
+		printf("  Found unloaded shared object in ./libs/ -> LOADING module %s\n", __FILE__);
+		fflush(stdout);
+	}	
 }
 
 
